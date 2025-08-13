48e540e32807dd6067dae62326b17fe1bb215637
NajmulHasanNoman <noman.cse.ju@gmail.com>
2025-08-13 23:08:48 +0600

    otp sending tested and mail sent

--- src/utility/EmailUtility.js
+++ src/utility/EmailUtility.js
@@ -1,11 +1,18 @@
-import React from 'react';
 
-const EmailUtility = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default EmailUtility;
+import nodemailer from 'nodemailer';
+export async function SendEmail(EmailTo, EmailText,EmailSubject) {
+    let Transport = nodemailer.createTransport({
+        host:"smtp.gmail.com",
+        port:25,
+        secure:false,
+        auth:{user:"noman.cse.eu@gmail.com", pass:"vdyd oynb emmq lvsf"},
+        tls:{rejectUnauthorized:false}
+    })
+    let MailOption={
+        from:"Next Ecommerce<noman.cse.eu@gmail.com>",
+        to:EmailTo,
+        subject:EmailSubject,
+        text:EmailText
+    }
+    return await Transport.sendMail(MailOption)
+}
