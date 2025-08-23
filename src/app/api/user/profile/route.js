cfc3c15d5c84e15c8c40adf26fd736ba5a439de4
NajmulHasanNoman <noman.cse.ju@gmail.com>
2025-08-23 22:03:28 +0600

    individual user profile view api added

--- src/app/api/user/profile/route.js
+++ src/app/api/user/profile/route.js
@@ -1,13 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"; 
 
 export async function POST(req,res) {
       try{
         let headerList=await headers();
         let id= parseInt(headerList.get('id'));
         
-        
+       // let id=4;
         //for just checking whether it comes correctly from headers after verifying token
         //let email= (headerList.get('email'));
         // console.log(id,email)
@@ -25,4 +25,19 @@ export async function POST(req,res) {
       catch(e){
         NextResponse.json({status:"fail",data:e.toString()})
       }
+}
+export async function GET(req,res) {
+  try{
+    let headerList=await headers();
+    let id=parseInt(headerList.get('id'));
+
+    const prisma=new PrismaClient();
+    const result=await prisma.customer_profiles.findUnique({
+      where:{user_id:id}
+    })
+    return NextResponse.json({status:"success",data:result})
+  }
+  catch(e){
+    return NextResponse.json({status:"fail",data:e.toString()})
+  }
 }
