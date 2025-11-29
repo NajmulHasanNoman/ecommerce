181d9285a4e28cf7dba4c3d4e94d161dd7be29aa
NajmulHasanNoman <noman.cse.ju@gmail.com>
2025-11-29 19:54:13 +0600

    features added

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,23 @@
 "use client";
 import React from 'react';
 import Master from '@/components/master/Master';
-import SliderSkeleton from '@/skeleton/sliderSkeleton';
-import FeaturesSkeleton from '@/skeleton/featuresSkeleton';
+// import SliderSkeleton from '@/skeleton/sliderSkeleton';
+import Slider from '@/components/product/slider';
+
+// import FeaturesSkeleton from '@/skeleton/featuresSkeleton';
 import BrandsSkeleton from '@/skeleton/brandsSkeleton';
 import ProductsSkeleton from '@/skeleton/productsSkeleton';
 import CategoriesSkeleton from '@/skeleton/categoriesSkeleton';
+import Features from '@/components/features/features';
 const page = () => {
   return (
     <div>
       <Master>
-      <SliderSkeleton/>
-      <FeaturesSkeleton/>
+      {/* <SliderSkeleton/> */}
+      <Slider/>
+      <Features/>
+      
+      {/* <FeaturesSkeleton/> */}
       <BrandsSkeleton/>
       <ProductsSkeleton/>
       <CategoriesSkeleton/> 
